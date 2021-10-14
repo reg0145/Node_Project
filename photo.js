46f67823e060c6808dae9708362a67629fdcd1bc
reg0145 <reg0145@naver.com>
2021-10-15 02:37:41 +0900

    'join'

--- photo.js
+++ photo.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
-import path from 'path'
+import path, {join} from 'path'
 
 if (process.argv.length == 3){
-    var dir = path.join(path.dirname(process.argv[1]), process.argv[2]);
+    var dir = join(path.dirname(process.argv[1]), process.argv[2]);
     arrangeFile(dir);
 }
 
@@ -13,13 +13,13 @@ function arrangeFile(dir){
       .then((files) => {  
         files.forEach((file) => {
             if (file.match(/.(png|aae)$/)){    
-                moveFile(path.join(dir, file),'captured');
+                moveFile(join(dir, file),'captured');
             }
             else if (file.match(/.(mp4|mov|avi|wmv)$/)){
-                moveFile(path.join(dir, file),'video');
+                moveFile(join(dir, file),'video');
             }
             else if (file.match(/^IMG_E.*(jpg|jpeg)$/)){
-                var originalPath = path.join(dir, 'IMG_' + file.split('IMG_E')[1]);
+                var originalPath = join(dir, 'IMG_' + file.split('IMG_E')[1]);
                 try{
                     if (fs.statSync(originalPath).isFile())
                         moveFile(originalPath, 'duplicated');
@@ -36,10 +36,10 @@ function arrangeFile(dir){
 function moveFile(file, movedir){
     var filedir = path.dirname(file);
     var filename = path.basename(file);
-    fs.mkdir(path.join(filedir, movedir), { recursive:true } , (error) =>{
+    fs.mkdir(join(filedir, movedir), { recursive:true } , (error) =>{
         if (error) console.error(error);
     });
-    fs.rename(file, path.join(filedir, movedir, filename), (error) =>{
+    fs.rename(file, join(filedir, movedir, filename), (error) =>{
         if (error) console.error(error);
     });
     console.log('move', filename, 'to', movedir);
